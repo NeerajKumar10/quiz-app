175df532d95346d6f99dcf504bb03269fd3d86f0
neeraj kumar <nkarya654@gmail.com>
2026-02-20 13:57:51 +0530

    Categries

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Models\Admin;
+use App\Models\Category;
 
 class AdminController extends Controller
 {
@@ -37,5 +38,30 @@ class AdminController extends Controller
             return redirect('admin-login');
         }
         
+    }
+    function categories(){
+        $categories=Category::get();
+          $admin =Session::get('admin');
+        if($admin){
+            return view('categories',["name"=>$admin->name,"categories"=>$categories]);
+        }else{
+            return redirect('admin-login');
+        }
+    }
+    function logout(){
+        Session::forget("admin");
+        return redirect("admin-login");
+    }
+    function addCategory(Request $request){
+        $admin =Session::get('admin');
+        $category=new Category();
+        $category->name=$request->category;
+        $category->creator=$admin->name;
+
+        if($category->save()){
+            Session::flash('category',"Category ".$request->category." added");
+        }
+
+        return redirect("admin-categories");
     }
 }
